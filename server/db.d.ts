5b144c8c4afe9812b124483893de31f6373dec64
abhilashreddi <29952762-abhilashreddi@users.noreply.replit.com>
2025-03-16 18:54:24 +0000

    Restored to '5909d64526a57018ff3b6803098a6bb7c77e25af'
    Replit-Restored-To: 5909d64526a57018ff3b6803098a6bb7c77e25af

--- server/db.d.ts
+++
@@ -1,5 +0,0 @@
-import { Pool } from '@neondatabase/serverless';
-export declare const pool: Pool;
-export declare const db: import("drizzle-orm/neon-serverless").NeonDatabase<any> & {
-    $client: Pool;
-};
